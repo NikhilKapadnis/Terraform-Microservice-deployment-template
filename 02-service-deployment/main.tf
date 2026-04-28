decfc7a447812599ca42a933ba0cbf016db04829
NikhilKapadnis <nikhilkapadnis2000@gmail.com>
2026-04-29 00:08:45 +0100

    will not wait for steady state

--- 02-service-deployment/main.tf
+++ 02-service-deployment/main.tf
@@ -95,6 +95,8 @@ resource "aws_ecs_service" "service" {
   task_definition = aws_ecs_task_definition.task.arn
   desired_count   = var.desired_count
   launch_type     = "EC2"
+  
+  wait_for_steady_state = false
 
   load_balancer {
     target_group_arn = aws_lb_target_group.tg.arn
